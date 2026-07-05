8cdbc5136ee13faf46afa118a47094f3461612b1
Florian Innocente <innocente@mac.om>
2026-07-05 08:05:10 +0200

    Initial Commit

---
+++ PierreVincent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PierreVincent
+//
+//  Created by Florian Innocente on 05/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PierreVincent/PierreVincentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PierreVincentApp.swift
+//  PierreVincent
+//
+//  Created by Florian Innocente on 05/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct PierreVincentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
